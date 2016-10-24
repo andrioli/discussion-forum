c918638a3f469613d1d5f424e3848e726ca274f1
Roberto Araujo <roberto.andrioli@gmail.com>
2016-10-24 01:13:42 -0300

    Simple word censorship implementation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,20 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :email
   validates_presence_of :body
+
+  # Using this array constant as a black list of words
+  # Ideally it must be implemented as a database table or as a file
+  BLACKLIST = [
+    'fuck', 'shit', 'bullshit'
+  ]
+
+  # Custom getter, consider caching the return value
+  def body
+    censored_body = self[:body]
+    BLACKLIST.each do |word|
+      censored_body = censored_body.gsub(
+        /( |^)(#{word})( |$)/i, '\1' + '*' * word.length + '\3')
+    end unless censored_body.nil?
+    return censored_body
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new
+  end
+
+  test "blacklist works" do
+    @post.body = 'Fuck you'
+    assert_equal('**** you', @post.body)
+
+    @post.body = 'This is bullshit'
+    assert_equal('This is ********', @post.body)
+
+    @post.body = 'I dont give a shit'
+    assert_equal('I dont give a ****', @post.body)
+  end
 end
